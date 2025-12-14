2f2dc6580c0c7cc72f97fb0d33437d19203f3c91
Anna Czopko <aczopko@student.agh.edu.pl>
2025-12-14 15:16:22 +0100

    App.js updated with lenght title

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Wall-E';
+    // let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');  // definicja stanu, Wall-E to wartość startowa
+    const [description, setDescription] = useState('A movie about a robot');
+
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+        // console.log(event.target.value);
+        setTitle(event.target.value);
     }
+
+    const titleLength = title.trim().length;
+
+    let message = null;
+    if (titleLength > 0 && titleLength < 3) {
+        message = "Tytuł jest za krótki.";
+    } else if (titleLength >= 3 && titleLength < 10) {
+        message = "";
+    } else if (titleLength >= 10) {
+        message = "Tytuł jest bardzo długi.";
+    }
+
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
+            <h2>My favourite movie for today is {title}</h2>
             <h2>My favourite movie for today is {title.toUpperCase()}</h2>
-            <input type="text" onChange={handleChange}/>
+            {titleLength > 0 && (
+                <p style={{color: "steelblue"}}>{message}</p>
+            )}
+            <input type="text" value={title} onChange={handleChange}/>
         </div>
     );
 }
